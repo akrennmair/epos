cac085b91577fd547152cfa1815a75a88c9dd420
Andreas Krennmair <ak@synflood.at>
2012-11-27 00:14:37 +0100

    add benchmarks for insert, update, delete.

--- store_test.go
+++ store_test.go
@@ -29,3 +29,83 @@ func TestStore(t *testing.T) {
 		t.Errorf("db.Remove failed: %v", err)
 	}
 }
+
+var benchmarkData = struct {
+		Name string
+		Age uint
+		SSN string
+		LuckyNumbers []int
+	} {
+		Name: "John J. McWhackadoodle",
+		Age: 29,
+		SSN: "078-05-1120",
+		LuckyNumbers: []int{23, 43},
+	}
+
+func BenchmarkInsert(b *testing.B) {
+	b.StopTimer()
+
+	db, _ := OpenDatabase("testdb_bench_insert")
+
+	b.StartTimer()
+
+	for i := 0 ; i < b.N ; i++ {
+		_, err := db.Coll("bench").Insert(benchmarkData)
+		if err != nil {
+			b.Fatal("insert failed: ", err)
+		}
+	}
+
+	b.StopTimer()
+	db.Close()
+	db.Remove()
+}
+
+func BenchmarkUpdate(b *testing.B) {
+	b.StopTimer()
+
+	db, _ := OpenDatabase("testdb_bench_update")
+
+	id, err := db.Coll("bench").Insert(benchmarkData)
+	if err != nil {
+		b.Fatal("insert failed: ", err)
+	}
+
+	b.StartTimer()
+
+	for i := 0; i < b.N ; i++ {
+		benchmarkData.LuckyNumbers[0], benchmarkData.LuckyNumbers[1] = benchmarkData.LuckyNumbers[1], benchmarkData.LuckyNumbers[0]
+		if err = db.Coll("bench").Update(id, data); err != nil {
+			b.Fatal("update failed: ", err)
+		}
+	}
+
+	b.StopTimer()
+	db.Close()
+	db.Remove()
+}
+
+func BenchmarkDelete(b *testing.B) {
+	b.StopTimer()
+
+	db, _ := OpenDatabase("testdb_bench_delete")
+
+	b.StartTimer()
+
+	for i := 0 ; i < b.N ; i++ {
+		b.StopTimer()
+		id, err := db.Coll("bench").Insert(benchmarkData)
+		if err != nil {
+			b.Fatal("insert failed: ", err)
+		}
+		b.StartTimer()
+		if err = db.Coll("bench").Delete(id); err != nil {
+			b.Fatal("delete failed: ", err)
+		}
+	}
+
+	b.StopTimer()
+	db.Close()
+	db.Remove()
+
+}
